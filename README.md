1788baa402f63ce462d800666fbf0fe610fc1b74
koushiroue <koushiroue@gmail.com>
2021-05-17 16:49:00 +0800

    learning

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sylvette
+# Sylvette - Your comfy childhood-friend-like bot  
+
+![anxiety](assets/github/main.png)  
+
+work in progress
+
+Discord.js packages in `package.json` :  
+
+- `discord.js` (obviously)  
+- `dotenv`
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+//Main js file.
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-require('dotenv').config();
+//config
 const { prefix } = require('./config.json');
+const { token } = require('./config.json');
 
+//token
+require('dotenv').config();
+
+//cli prepping
 client.once('ready', () => {
 	console.log('aight.');
 });
+//ping a.k.a hello world
 client.on('message', message => {
 	if (message.content.startsWith(`${prefix}ping`)) {
 		message.channel.send('Pong.');
 	} else if (message.content.startsWith(`${prefix}beep`)) {
 		message.channel.send('Boop.');
 	}
+	if (message.content.startsWith(`${prefix}help`)){
+		message.channel.send('you asked for help but nobody came.');
+	}
 });
+//status
+// Set the bot's presence (activity and status)
+client.on("ready", () => {
+    client.user.setPresence({
+        activity: { 
+            name: 'Cels cringing.',
+            type: 'WATCHING'
+        },
+        status: 'idle'
+    })
+})
+//auth
 client.login(process.env.TOKEN);
